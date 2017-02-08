dd0800e91c63b040ef0efe19aa62df5fdb84a6ab
pierre.vuckovic <pierre.vuckovic@P209.serform.local>
2017-02-08 09:31:48 +0100

    1er essai

---
+++ src/memory/Plateau.java
@@ -0,0 +1,26 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package memory;
+
+/**
+ *
+ * @author pierre.vuckovic
+ */
+public class Plateau {
+    // ATTRIBUTS
+    private Emplacement[][] cartes;
+    private int[] nbCartes;
+    private Emplacement[] placesVides;
+    private int nbPlacesVides;
+    
+    // CONSTRUCTEURS
+    
+
+    
+    // METHODES
+    
+    
+    // GETTERS
+}
